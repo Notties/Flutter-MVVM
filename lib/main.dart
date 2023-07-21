8a676ed7b34072ecb1dc18d9d2a38dd1a2e3f63b
Knot <akthakorn@gmail.com>
2023-07-21 21:02:33 +0700

    update name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mvvm/home_screen.dart';
+import 'package:flutter_mvvm/users_list/views/home_screen.dart';
+import 'package:flutter_mvvm/users_list/view_models/users_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,15 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MVVM',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-        brightness: Brightness.dark,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
-      home: HomeScreen(),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => UsersViewModel(),
+          )
+        ],
+        child: MaterialApp(
+          title: 'MVVM',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primaryColor: Colors.blue,
+              brightness: Brightness.light,
+              visualDensity: VisualDensity.adaptivePlatformDensity),
+          home: HomeScreen(),
+        ));
   }
 }
